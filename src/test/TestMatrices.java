82c65f03f9ba700dc7e6fd8d82553bfce940958e
Samantha González <samujm76@gmail.com>
2022-08-14 18:57:25 -0500

    Instanciando Matriz

--- src/test/TestMatrices.java
+++ src/test/TestMatrices.java
@@ -2,6 +2,9 @@ package test;
 
 public class TestMatrices {
     public static void main(String[] args) {
-        
+        /*--------------------------------------- Creando la matriz ----------------------------------------------*/
+        //Instanciando un objeto de tipo matriz
+        int edades[][] = new int[3][2]; //[renglones][columnas]
+        System.out.println("edades = " + edades);
     }
 }
