116024050ed4e654c03b8f72649572cc2f478571
Samantha González <samujm76@gmail.com>
2022-08-14 18:58:03 -0500

    ARREGLO DE ARREGLOS

--- src/test/TestMatrices.java
+++ src/test/TestMatrices.java
@@ -2,7 +2,7 @@ package test;
 
 public class TestMatrices {
     public static void main(String[] args) {
-        /*--------------------------------------- Creando la matriz ----------------------------------------------*/
+        /*--------------------------------------- Creando la matriz (ARREGLO DE ARREGLOS) ----------------------------------------------*/
         //Instanciando un objeto de tipo matriz
         int edades[][] = new int[3][2]; //[renglones][columnas]
         System.out.println("edades = " + edades);
