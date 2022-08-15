fce8df9d051cc0f225e2aed112c569afc2250ec1
Samantha González <samujm76@gmail.com>
2022-08-14 19:04:54 -0500

    Asignar valores a la matriz

--- src/test/TestMatrices.java
+++ src/test/TestMatrices.java
@@ -6,5 +6,22 @@ public class TestMatrices {
         //Instanciando un objeto de tipo matriz
         int edades[][] = new int[3][2]; //[renglones][columnas]
         System.out.println("edades = " + edades);
+
+        /*--------------------------------------- Asignar valores a la matriz ---------------------------------------------------------*/
+        edades[0][0] = 5;
+        edades[0][1] = 7;
+        edades[1][0] = 8;
+        edades[1][1] = 4;
+        edades[2][0] = 9;
+        edades[2][1] = 2;
+
+        System.out.println("edades [0][0]= " + edades[0][0]);
+        System.out.println("edades [0][1]= " + edades[0][1]);
+        System.out.println("edades [1][0]= " + edades[1][0]);
+        System.out.println("edades [1][1]= " + edades[1][1]);
+        System.out.println("edades [2][0]= " + edades[2][0]);
+        System.out.println("edades [2][1]= " + edades[2][1]);
+
+
     }
 }
